acb2101f06546c8980122b0e09e8f2f4f1ac3908
SuperBussee <tenz134120@gmail.com>
2024-05-24 15:51:10 +0700

    first commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    //Create integer variable
+    int age = 16;
+    //Print Hello to the screen
+    printf("Hellon world!\n");
+    printf("I  Am the RonalSpeed\n");
+    printf("Age: %d\n",age);
+    return 0;
+}
